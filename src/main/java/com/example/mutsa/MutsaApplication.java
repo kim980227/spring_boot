4bf416e3a6d95cfa6bc0aa7fe3fd07cb260c42d4
kim980227 <sear227@gmail.com>
2023-05-25 21:18:42 +0900

    first commit

---
+++ src/main/java/com/example/mutsa/MutsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mutsa/MutsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
